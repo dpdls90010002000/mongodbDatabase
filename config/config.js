991342b0a9642f429085021acb43f35f277b98e1
dpdls90010002000 <dpdls9001000@gmail.com>
2023-10-17 10:49:32 -0400

    first commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+
+const config = {
+    env: process.env.NODE_ENV || 'development', 
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://dpdls300:HBek6i66lX7noA3W@yeinan.zy7tzqm.mongodb.net/HatStore"
+  };
+export default config
